2eb67cb02fa9107d53e1f5d996cd26a3da8fe0c5
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-02-25 20:55:53 +0700

    change something

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import type { NextConfig } from "next";
 
 const config: NextConfig = {
-  // Remove 'output: "export"' if present
   typescript: {
     ignoreBuildErrors: true, // Optional: if you have TypeScript errors during build
   },
