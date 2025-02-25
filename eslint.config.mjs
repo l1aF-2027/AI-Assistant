1c8d4942cd4947baf978659c3d6b1f4386a0f196
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-02-25 20:23:07 +0700

    change eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,11 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+{
+  "extends": "next/core-web-vitals",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@typescript-eslint/quotes": "off",
+    "quotes": [0]
+    "avoidEscape": "off",
+    "allowTemplateLiterals": 0,
+    "no-useless-escape":0,
+  }
+}
