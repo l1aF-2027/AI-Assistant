c34f08989c5b9a539379bc95f4e1fbbd9e198a6a
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-22 15:27:49 +0700

    Added Google Search Console verification file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata = {
   title: "Comet",
-  icons:
-  {
-    icon: 'web.ico',
-  }
+  icons: {
+    icon: "web.ico",
+  },
+  description:
+    "The AI Assistant website is an intelligent platform that provides an AI-powered virtual assistant with a wide range of features to support users in their daily tasks.",
+  keywords:
+    "AI Assistant, AI-powered virtual assistant, AI assistant website, AI assistant platform, AI assistant features, AI assistant tasks, Comet, Comet AI assistant, AI assistant by l1af-2027, AI assistant by l1af-2027 Comet",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -19,9 +21,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
